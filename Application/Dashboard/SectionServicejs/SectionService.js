4847079e39f2856607e124ece3c5f0a638d1f647
yasin <ysndlklc1234@gmail.com>
2018-11-17 12:04:37 +0300

    servislerde düzenleme yapıldı

--- Application/Dashboard/SectionServicejs/SectionService.js
+++ Application/Dashboard/SectionServicejs/SectionService.js
@@ -8,46 +8,4 @@ var SectionService = {
         })
         return deferred;
     },
-    // get: function (json) {
-    //     var deferred = new Promise(function (resolve, reject) {
-    //         $.ajax({
-    //             type: "POST",
-    //             url: "/okul/AllService/ServiceReq.php",
-    //             datatype: 'application/json',
-    //             data: json,
-    //             success: function (data, status, xhr) {
-    //                 if (!data.length) {
-    //                     resolve(data.status);
-    //                 } else {
-    //                     resolve(data);
-    //                 }
-    //             },
-    //             error: function (data, status, xhr) {
-    //                 resolve("")
-    //             }
-    //         });
-    //     });
-    //     return deferred;
-    // },
-    // add: function (json) {
-    //     var deferred = new Promise(function (resolve, reject) {
-    //         $.ajax({
-    //             type: "POST",
-    //             url: "/okul/AllService/ServiceReq.php",
-    //             datatype: 'application/json',
-    //             data: json,
-    //             success: function (data, status, xhr) {
-    //                 if (!data.length) {
-    //                     resolve(data.status);
-    //                 } else {
-    //                     resolve(data);
-    //                 }
-    //             },
-    //             error: function (data, status, xhr) {
-    //                 resolve("")
-    //             }
-    //         });
-    //     });
-    //     return deferred;
-    // }
 }
